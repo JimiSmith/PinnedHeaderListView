efed0bad6fd4ff9f16bd8e5dc61d2440cd9c2038
James Smith <smithj002@gmail.com>
2013-05-11 11:24:30 +0200

    Simplify the sample a bit

--- example/src/za/co/immedia/pinnedheaderlistviewexample/MainActivity.java
+++ example/src/za/co/immedia/pinnedheaderlistviewexample/MainActivity.java
@@ -1,12 +1,7 @@
 package za.co.immedia.pinnedheaderlistviewexample;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import za.co.immedia.pinnedheaderlistview.PinnedHeaderListView;
 
 public class MainActivity extends Activity {
@@ -16,23 +11,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         PinnedHeaderListView listView = (PinnedHeaderListView) findViewById(R.id.pinnedListView);
-        LayoutInflater inflator = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        LinearLayout header1 = (LinearLayout) inflator.inflate(R.layout.list_item, null);
-        ((TextView) header1.findViewById(R.id.textItem)).setText("HEADER 1");
-        LinearLayout header2 = (LinearLayout) inflator.inflate(R.layout.list_item, null);
-        ((TextView) header2.findViewById(R.id.textItem)).setText("HEADER 2");
-        LinearLayout footer = (LinearLayout) inflator.inflate(R.layout.list_item, null);
-        ((TextView) footer.findViewById(R.id.textItem)).setText("FOOTER");
-        listView.addHeaderView(header1);
-        listView.addHeaderView(header2);
-        listView.addFooterView(footer);
         TestSectionedAdapter sectionedAdapter = new TestSectionedAdapter();
         listView.setAdapter(sectionedAdapter);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
     }
 }
